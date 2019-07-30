afe16684c8fba386e00ffef0829eda78383bbfcd
MatthewNobes <mattnobes.personal@protonmail.com>
2019-07-30 15:07:15 +0100

    Setting up form design for first version and adding in comparison codes for the hash algorithms.

--- Hash Checker/Form1.Designer.cs
+++ Hash Checker/Form1.Designer.cs
@@ -28,20 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grbHashInfo = new System.Windows.Forms.GroupBox();
+            this.cmbAlgorithm = new System.Windows.Forms.ComboBox();
+            this.lblAlgorithm = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.txtOriginalHash = new System.Windows.Forms.TextBox();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.lblOriginalHash = new System.Windows.Forms.Label();
+            this.grbResult = new System.Windows.Forms.GroupBox();
+            this.txtSafeToUse = new System.Windows.Forms.TextBox();
+            this.lblSafeToUse = new System.Windows.Forms.Label();
+            this.lblResultHash = new System.Windows.Forms.Label();
+            this.txtResultHash = new System.Windows.Forms.TextBox();
+            this.grbHashInfo.SuspendLayout();
+            this.grbResult.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grbHashInfo
+            // 
+            this.grbHashInfo.Controls.Add(this.cmbAlgorithm);
+            this.grbHashInfo.Controls.Add(this.lblAlgorithm);
+            this.grbHashInfo.Controls.Add(this.lblFilePath);
+            this.grbHashInfo.Controls.Add(this.txtOriginalHash);
+            this.grbHashInfo.Controls.Add(this.txtFilePath);
+            this.grbHashInfo.Controls.Add(this.lblOriginalHash);
+            this.grbHashInfo.Location = new System.Drawing.Point(13, 13);
+            this.grbHashInfo.Name = "grbHashInfo";
+            this.grbHashInfo.Size = new System.Drawing.Size(358, 183);
+            this.grbHashInfo.TabIndex = 0;
+            this.grbHashInfo.TabStop = false;
+            this.grbHashInfo.Text = "Hash Information";
+            // 
+            // cmbAlgorithm
+            // 
+            this.cmbAlgorithm.FormattingEnabled = true;
+            this.cmbAlgorithm.Items.AddRange(new object[] {
+            "MD5",
+            "SHA-1",
+            "SHA-2",
+            "SHA-256",
+            "SHA-512"});
+            this.cmbAlgorithm.Location = new System.Drawing.Point(83, 149);
+            this.cmbAlgorithm.Name = "cmbAlgorithm";
+            this.cmbAlgorithm.Size = new System.Drawing.Size(269, 24);
+            this.cmbAlgorithm.TabIndex = 4;
+            // 
+            // lblAlgorithm
+            // 
+            this.lblAlgorithm.AutoSize = true;
+            this.lblAlgorithm.Location = new System.Drawing.Point(10, 152);
+            this.lblAlgorithm.Name = "lblAlgorithm";
+            this.lblAlgorithm.Size = new System.Drawing.Size(71, 17);
+            this.lblAlgorithm.TabIndex = 3;
+            this.lblAlgorithm.Text = "Algorithm:";
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(10, 124);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(67, 17);
+            this.lblFilePath.TabIndex = 2;
+            this.lblFilePath.Text = "File Path:";
+            // 
+            // txtOriginalHash
+            // 
+            this.txtOriginalHash.Location = new System.Drawing.Point(10, 42);
+            this.txtOriginalHash.Multiline = true;
+            this.txtOriginalHash.Name = "txtOriginalHash";
+            this.txtOriginalHash.Size = new System.Drawing.Size(342, 73);
+            this.txtOriginalHash.TabIndex = 1;
+            this.txtOriginalHash.TextChanged += new System.EventHandler(this.TxtOriginalHash_TextChanged);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(83, 121);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(269, 22);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // lblOriginalHash
+            // 
+            this.lblOriginalHash.AutoSize = true;
+            this.lblOriginalHash.Location = new System.Drawing.Point(7, 22);
+            this.lblOriginalHash.Name = "lblOriginalHash";
+            this.lblOriginalHash.Size = new System.Drawing.Size(98, 17);
+            this.lblOriginalHash.TabIndex = 0;
+            this.lblOriginalHash.Text = "Original Hash:";
+            // 
+            // grbResult
+            // 
+            this.grbResult.Controls.Add(this.txtSafeToUse);
+            this.grbResult.Controls.Add(this.lblSafeToUse);
+            this.grbResult.Controls.Add(this.lblResultHash);
+            this.grbResult.Controls.Add(this.txtResultHash);
+            this.grbResult.Location = new System.Drawing.Point(13, 202);
+            this.grbResult.Name = "grbResult";
+            this.grbResult.Size = new System.Drawing.Size(358, 157);
+            this.grbResult.TabIndex = 2;
+            this.grbResult.TabStop = false;
+            this.grbResult.Text = "Result";
+            // 
+            // txtSafeToUse
+            // 
+            this.txtSafeToUse.Location = new System.Drawing.Point(98, 22);
+            this.txtSafeToUse.Name = "txtSafeToUse";
+            this.txtSafeToUse.Size = new System.Drawing.Size(254, 22);
+            this.txtSafeToUse.TabIndex = 5;
+            // 
+            // lblSafeToUse
+            // 
+            this.lblSafeToUse.AutoSize = true;
+            this.lblSafeToUse.Location = new System.Drawing.Point(7, 22);
+            this.lblSafeToUse.Name = "lblSafeToUse";
+            this.lblSafeToUse.Size = new System.Drawing.Size(84, 17);
+            this.lblSafeToUse.TabIndex = 4;
+            this.lblSafeToUse.Text = "Safe to use:";
+            // 
+            // lblResultHash
+            // 
+            this.lblResultHash.AutoSize = true;
+            this.lblResultHash.Location = new System.Drawing.Point(10, 52);
+            this.lblResultHash.Name = "lblResultHash";
+            this.lblResultHash.Size = new System.Drawing.Size(89, 17);
+            this.lblResultHash.TabIndex = 3;
+            this.lblResultHash.Text = "Result Hash:";
+            // 
+            // txtResultHash
+            // 
+            this.txtResultHash.Location = new System.Drawing.Point(10, 75);
+            this.txtResultHash.Multiline = true;
+            this.txtResultHash.Name = "txtResultHash";
+            this.txtResultHash.Size = new System.Drawing.Size(342, 73);
+            this.txtResultHash.TabIndex = 2;
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(383, 369);
+            this.Controls.Add(this.grbResult);
+            this.Controls.Add(this.grbHashInfo);
             this.Name = "mainForm";
             this.Text = "Hash Checker";
+            this.grbHashInfo.ResumeLayout(false);
+            this.grbHashInfo.PerformLayout();
+            this.grbResult.ResumeLayout(false);
+            this.grbResult.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grbHashInfo;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.TextBox txtOriginalHash;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Label lblOriginalHash;
+        private System.Windows.Forms.GroupBox grbResult;
+        private System.Windows.Forms.TextBox txtResultHash;
+        private System.Windows.Forms.ComboBox cmbAlgorithm;
+        private System.Windows.Forms.Label lblAlgorithm;
+        private System.Windows.Forms.TextBox txtSafeToUse;
+        private System.Windows.Forms.Label lblSafeToUse;
+        private System.Windows.Forms.Label lblResultHash;
     }
 }
 
--- Hash Checker/Form1.cs
+++ Hash Checker/Form1.cs
@@ -15,6 +15,56 @@ namespace Hash_Checker
         public mainForm()
         {
             InitializeComponent();
+
+
+        }
+
+        private void TxtOriginalHash_TextChanged(object sender, EventArgs e)
+        {
+       
+            if (cmbAlgorithm.Text == "MD5")
+            {
+                MessageBox.Show("md5 work");
+
+            }
+            else if (cmbAlgorithm.Text == "SHA-1")
+            {
+                MessageBox.Show("SHA-1");
+            }
+            else if (cmbAlgorithm.Text == "SHA-2")
+            {
+                MessageBox.Show("SHA-2");
+            }
+            else if (cmbAlgorithm.Text == "SHA-256")
+            {
+                MessageBox.Show("SHA-256");
+            }
+            else if (cmbAlgorithm.Text == "SHA-512")
+            {
+                MessageBox.Show("SHA-512");
+            }
+        }
+        
+        public static bool HashComparison(this String newHash, String OriginalHash)
+        {
+            bool Match = false;
+            if (OriginalHash == newHash)
+            {
+                return Match = true;
+            }
+            else
+            {
+                return Match;
+            }
+        }
+    }
+
+    public static class HashChecks
+    {
+        public static String MD5Check(this string newHash)
+        {
+
+            return newHash;
         }
     }
 }
